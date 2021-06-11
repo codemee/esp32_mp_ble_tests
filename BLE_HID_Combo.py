b97ce9174d588492f7750dbb51c21048431d7d4f
meebox <meebox@gmail.com>
2021-06-11 13:38:06 +0800

    update to keyboard/comsumer control combo HID

--- BLE_HID_Combo.py
+++ BLE_HID_Combo.py
@@ -1,8 +1,61 @@
-# Implements a BLE HID keyboard
+# implements a BLE HID keyboard with media keys
 
-from micropython import const
+from machine import Pin
+import time
 import struct
 import bluetooth
+
+# report map for HID
+# https://www.usb.org/document-library/hid-usage-tables-122
+HID_REPORT_MAP = bytes([
+    # report map for keyboard
+    0x05, 0x01,  # Usage Page (Generic Desktop)
+    0x09, 0x06,  # Usage (Keyboard)
+    0xA1, 0x01,  # Collection (Application)
+    0x85, 0x01,   # Report ID (1)
+    0x75, 0x01,   # Report Size (1)
+    0x95, 0x08,   # Report Count (8)
+    0x05, 0x07,   # Usage Page (Key Codes)
+    0x19, 0xE0,   # Usage Minimum (224)
+    0x29, 0xE7,   # Usage Maximum (231)
+    0x15, 0x00,   # Logical Minimum (0)
+    0x25, 0x01,   # Logical Maximum (1)
+    0x81, 0x02,   # Input (Data, Variable, Absolute); Modifier byte
+    0x95, 0x01,   # Report Count (1)
+    0x75, 0x08,   # Report Size (8)
+    0x81, 0x01,   # Input (Constant); Reserved byte
+    0x95, 0x05,   # Report Count (5)
+    0x75, 0x01,   # Report Size (1)
+    0x05, 0x08,   # Usage Page (LEDs)
+    0x19, 0x01,   # Usage Minimum (1)
+    0x29, 0x05,   # Usage Maximum (5)
+    0x91, 0x02,   # Output (Data, Variable, Absolute); LED report
+    0x95, 0x01,   # Report Count (1)
+    0x75, 0x03,   # Report Size (3)
+    0x91, 0x01,   # Output (Constant); LED report padding
+    0x95, 0x06,   # Report Count (6)
+    0x75, 0x08,   # Report Size (8)
+    0x15, 0x00,   # Logical Minimum (0)
+    0x25, 0x65,   # Logical Maximum (101)
+    0x05, 0x07,   # Usage Page (Key Codes)
+    0x19, 0x00,   # Usage Minimum (0)
+    0x29, 0x65,   # Usage Maximum (101)
+    0x81, 0x00,   # Input (Data, Array); Key array (6 bytes)
+    0xC0,       # End Collection
+    # report map for Cosumer Control (media keys)
+    0x05, 0x0C,  # Usage Page (CONSUMER PAGE)
+    0x09, 0x01,  # Usage (Consumer Control)
+    0xA1, 0x01,  # Collection (Application)
+    0x85, 0x02,   # here Report ID (2)
+    0x75, 0x10,   # Report Size (16)
+    0x95, 0x01,   # Report Count (1)
+    0x15, 0x01,   # Logical Minimum (1)
+    0x26, 0x8C, 0x02,  # Logical Maximum (0x028C)
+    0x19, 0x01,   # Usage Minimum (1)
+    0x2A, 0x8C, 0x02,  # Usage Maximum (0x028C)
+    0x81, 0x00,   # Input (Data, Array, Absolute); Modifier byte
+    0xC0,       # End Collection
+])
 
 
 def ble_irq(event, data):
@@ -24,103 +77,90 @@ F_READ = bluetooth.FLAG_READ
 F_WRITE = bluetooth.FLAG_WRITE
 F_READ_WRITE = bluetooth.FLAG_READ | bluetooth.FLAG_WRITE
 F_READ_NOTIFY = bluetooth.FLAG_READ | bluetooth.FLAG_NOTIFY
+F_READ_WRITE_NORESPONSE = bluetooth.FLAG_READ | bluetooth.FLAG_WRITE | bluetooth.FLAG_WRITE_NO_RESPONSE
 
 ATT_F_READ = 0x01
 ATT_F_WRITE = 0x02
-
-# fmt: off
-HID_REPORT_MAP = bytes([
-    0x05, 0x01,     # Usage Page (Generic Desktop)
-    0x09, 0x06,     # Usage (Keyboard)
-    0xA1, 0x01,     # Collection (Application)
-    0x85, 0x01,     #     Report ID (1)
-    0x75, 0x01,     #     Report Size (1)
-    0x95, 0x08,     #     Report Count (8)
-    0x05, 0x07,     #     Usage Page (Key Codes)
-    0x19, 0xE0,     #     Usage Minimum (224)
-    0x29, 0xE7,     #     Usage Maximum (231)
-    0x15, 0x00,     #     Logical Minimum (0)
-    0x25, 0x01,     #     Logical Maximum (1)
-    0x81, 0x02,     #     Input (Data, Variable, Absolute); Modifier byte
-    0x95, 0x01,     #     Report Count (1)
-    0x75, 0x08,     #     Report Size (8)
-    0x81, 0x01,     #     Input (Constant); Reserved byte
-    0x95, 0x05,     #     Report Count (5)
-    0x75, 0x01,     #     Report Size (1)
-    0x05, 0x08,     #     Usage Page (LEDs)
-    0x19, 0x01,     #     Usage Minimum (1)
-    0x29, 0x05,     #     Usage Maximum (5)
-    0x91, 0x02,     #     Output (Data, Variable, Absolute); LED report
-    0x95, 0x01,     #     Report Count (1)
-    0x75, 0x03,     #     Report Size (3)
-    0x91, 0x01,     #     Output (Constant); LED report padding
-    0x95, 0x06,     #     Report Count (6)
-    0x75, 0x08,     #     Report Size (8)
-    0x15, 0x00,     #     Logical Minimum (0)
-    0x25, 0x65,     #     Logical Maximum (101)
-    0x05, 0x07,     #     Usage Page (Key Codes)
-    0x19, 0x00,     #     Usage Minimum (0)
-    0x29, 0x65,     #     Usage Maximum (101)
-    0x81, 0x00,     #     Input (Data, Array); Key array (6 bytes)
-    0xC0,           # End Collection
-    #here-----把鍵盤和多媒體的 HID_REPORT_MAP 並在一起
-    0x05, 0x0C,     # Usage Page (CONSUMER PAGE)
-    0x09, 0x01,     # Usage (Consumer Control)
-    0xA1, 0x01,     # Collection (Application)
-    0x85, 0x02,     #     here Report ID (2)
-    0x75, 0x10,     #     Report Size (16)
-    0x95, 0x01,     #     Report Count (1)
-    0x15, 0x01,     #     Logical Minimum (0)
-    0x26,
-    0x8C, 0x02,     #     Logical Maximum (1)
-    0x19, 0x01,     #     Usage Minimum (224)
-    0x2A,
-    0x8C, 0x02,     #     Usage Maximum (231)
-    0x81, 0x00,     #     Input (Data, Variable, Absolute); Modifier byte
-    0xC0,           # End Collection
-])
-# fmt: on
+ATT_F_READ_WRITE = ATT_F_READ | ATT_F_WRITE
 
 # 建立伺服器
-hid_service = (
-    UUID(0x1812),  # Human Interface Device            人機介面設備
-    (
-        (UUID(0x2A4A), F_READ),  # HID information     HID信息
-        (UUID(0x2A4B), F_READ),  # HID report map      HID報告圖
-        (UUID(0x2A4C), F_WRITE),  # HID control point  HID控制點
-        (UUID(0x2A4D), F_READ_NOTIFY, (
-            (UUID(0x2908), ATT_F_READ),)
-        ),  # Keyboard HID report / reference
-        (UUID(0x2A4D), F_READ_WRITE, (
-            (UUID(0x2908), ATT_F_READ),)
-        ),  # Keyboard status HID report / reference
-        (UUID(0x2A4D), F_READ_NOTIFY, (
-            (UUID(0x2908), ATT_F_READ),)
-        ),  # here Consumer Control HID report / reference
-        (UUID(0x2A4E), F_READ_WRITE),  # HID protocol mode
+hid_service = (                                   # 服務
+    UUID(0x1812), (                               # Human Interface Device
+        (UUID(0x2A4A), F_READ),                     # HID information
+        (UUID(0x2A4B), F_READ),                     # HID report map
+        (UUID(0x2A4C), F_WRITE),                    # HID control point
+        (UUID(0x2A4D), F_READ_NOTIFY, (             # Input report
+            (UUID(0x2908), ATT_F_READ),               # Report reference
+            # Client Characteristic Configuration
+            (UUID(0x2902), ATT_F_READ),
+        )),
+        (UUID(0x2A4D), F_READ_WRITE_NORESPONSE, (   # Output report
+            (UUID(0x2908), ATT_F_READ),               # Report reference
+        )),
+        (UUID(0x2A4D), F_READ_NOTIFY, (             # Consumer Control report
+            (UUID(0x2908), ATT_F_READ),               # Report reference
+        )),
+        (UUID(0x2A4E), F_READ_WRITE),               # HID protocol mode
+    ),
+)
+
+devinfo_service = (
+    UUID(0x180a), (                               # device info service
+        (UUID(0x2a50), F_READ),                     # pnp Char
+        (UUID(0x2a29), F_READ),                     # manufacture name Char
+    ),
+)
+
+bat_service = (
+    UUID(0x180f), (                               # battery Service
+        (UUID(0x2A19), F_READ_NOTIFY, (             # Battery Level
+            # Client Characteristic Configuration
+            (UUID(0x2902), ATT_F_READ_WRITE),       # for notify
+            # Characteristic Presentation Format    
+            (UUID(0x2904), ATT_F_READ),
+        )),
     ),
 )
 
 # register services  註冊服務
 ble.config(gap_name="MP-keyboard")
-handles = ble.gatts_register_services((hid_service,))
+handles = ble.gatts_register_services((
+    hid_service,
+    devinfo_service,
+    bat_service,
+))
 print(handles)
-#here 多了 h_com 和 h_d3
-h_info, h_hid, _, h_rep, h_d1, _, h_d2, h_com, h_d3, h_proto = handles[0]
+# 依序對應到服務中定義的特徵與描述器
+h_info, h_map, _, h_rep, h_d1, _, _, h_d2, h_com, h_d3, h_proto = handles[0]
+h_dev, h_manu = handles[1]
+h_bat, _, h_fmt = handles[2]
 
 # set initial data
-ble.gatts_write(h_info, b"\x01\x01\x00\x02")  # HID info: ver=1.1, country=0, flags=normal
-ble.gatts_write(h_hid, HID_REPORT_MAP)  # HID report map
+# HID info: ver=1.11, country=0, flags=normal
+ble.gatts_write(h_info, b"\x11\x01\x00\x00")
+ble.gatts_write(h_map, HID_REPORT_MAP)  # HID report map
 ble.gatts_write(h_d1, struct.pack("<BB", 1, 1))  # report: id=1, type=input
 ble.gatts_write(h_d2, struct.pack("<BB", 1, 2))  # report: id=1, type=output
-ble.gatts_write(h_d3, struct.pack("<BB", 2, 1))  #here  report: id=2, type=input
-ble.gatts_write(h_proto, b"\x01")  # protocol mode: report
+ble.gatts_write(h_d3, struct.pack("<BB", 2, 1))  # report: id=2, type=input
+ble.gatts_write(h_proto, b"\x01")  # protocol mode: report (0 for boot mode)
 
-# advertise 廣告
+# format:UINT8
+ble.gatts_write(h_fmt, b'\x04\x00\xad\x27\x01\x00\x00') 
+ble.gatts_write(h_bat, b"\x64")  # battery level, always 100%
+
+# vendor id source 0x01 -> Bluetooth company identify
+# vendor id 0x02E5 -> Expressif
+# Product id 0xA111 -> the ESP32BleKyboard lib's prod id
+# product version 0x0210 -> V2.1.0
+ble.gatts_write(h_dev, b'\x01\xe5\x02\x11\xa1\x10\x02')
+ble.gatts_write(h_manu, b'Espressif')
+
+# advertise 廣告封包, 長度最多 32 bytes
+# https://docs.silabs.com/bluetooth/latest/general/adv-and-scanning/bluetooth-adv-data-basics
 adv = (
-    b"\x02\x01\x06"
-    b"\x03\x03\x12\x18"  # complete list of 16-bit service UUIDs: 0x1812
-    b"\x03\x19\xc1\x03"  # appearance: keyboard
+    b"\x02\x01\x06"        # flag: 0x1010,
+    b"\x03\x03\x12\x18"    # complete list of 16-bit service UUIDs: 0x1812
+    b"\x03\x19\xc1\x03"    # appearance: keyboard
     b"\x0c\x09MP-keyboard"  # complete local name(要與上面的gap_name一樣)
 )
 conn_handle = None
@@ -131,11 +171,16 @@ ble.gap_advertise(100_000, adv)
 # 可以查看以下對照表
 # https://circuitpython.readthedocs.io/projects/hid/en/latest/_modules/adafruit_hid/keycode.html
 # https://www.usb.org/sites/default/files/documents/hut1_12v2.pdf
+
+
 def send_keycode(mod, code):
     # here press the key, 控制鍵盤用 h_rep
-    ble.gatts_notify(conn_handle, h_rep, struct.pack("8B", mod, 0, code, 0, 0, 0, 0, 0))
+    ble.gatts_notify(conn_handle, h_rep,
+                     struct.pack("8B", mod, 0, code, 0, 0, 0, 0, 0))
     # release the key
-    ble.gatts_notify(conn_handle, h_rep, b"\x00\x00\x00\x00\x00\x00\x00\x00")
+    ble.gatts_notify(conn_handle, h_rep,
+                     b"\x00\x00\x00\x00\x00\x00\x00\x00")
+
 
 def send_char(char):
     if char == " ":
@@ -158,22 +203,24 @@ def send_str(st):
     for c in st:
         send_char(c)
 
+
 def send_media_code(code):   # 0x28:ENTER    0x46:Print Scrn
     # here 控制音量用 h_com
     ble.gatts_notify(conn_handle, h_com, struct.pack("2B", code, 0x00))
     ble.gatts_notify(conn_handle, h_com, b"\x00\x00")
 
+
 def vol_inc():
     send_media_code(0xE9)
+
 
 def vol_dec():
     send_media_code(0xEA)
 
+
 def screen_shot():   # 0x28:ENTER    0x46:Print Scrn
     send_keycode(0, 0x46)
 
-from machine import Pin
-import time
 
 b12_prev = True   # 是否按下按鈕
 b13_prev = True   # 是否按下按鈕
@@ -189,5 +236,5 @@ while True:
         vol_inc()
         print("大聲 ")
     b13_prev = b13.value()
-    
+
     time.sleep(0.05)
